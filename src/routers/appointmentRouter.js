46f382fd7faf917490490b6a5c6e8a2e824efd24
SkandarEverest <skandar.everest@gmail.com>
2024-06-09 16:35:17 +0700

    tambah create appointment lagi sementara

--- src/routers/appointmentRouter.js
+++ src/routers/appointmentRouter.js
@@ -6,6 +6,7 @@ const appointmentRouter = express.Router();
 appointmentRouter.use(authentication)
 appointmentRouter.get('/list', AppointmentController.GetUserAppointment);
 appointmentRouter.get('/history', AppointmentController.GetUserAppointmentHistory);
+appointmentRouter.post('/create', AppointmentController.CreateAppointment);
 appointmentRouter.patch('/update/:id', AppointmentController.UpdateAppointmentById);
 appointmentRouter.patch('/cancel/:id', AppointmentController.CancelAppointmentByID);
 appointmentRouter.patch('/complete/:id', AppointmentController.CompleteAppointmentByID);
